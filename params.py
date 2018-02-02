2d03eb393337091d878ffb1c2e52e557c1f04c6a
Jiaming Ke <JiamingKe@outlook.com>
2018-02-02 10:34:50 +0000

    Update params.py
    Fix the reported bugs.

--- params.py
+++ params.py
@@ -16,10 +16,10 @@ class Parameters:
         # Parameters of the scene
         self.color_plane0 =  1. * np.ones(3)
         self.color_plane1 =  0. * np.ones(3)
-        self.scene = [ObjectsLib.add_sphere([.75, .1, 1.], .6, [0., 0., 1.]),
-                      ObjectsLib.add_sphere([-.75, .1, 2.25], .6, [.5, .223, .5]),
-                      ObjectsLib.add_sphere([-2.75, .1, 3.5], .6, [1., .572, .184]),
-                      ObjectsLib.add_plane([0., -.5, 0.], [0., 1., 0.],self.color_plane0, self.color_plane1),
+        self.scene = [objlib.add_sphere([.75, .1, 1.], .6, [0., 0., 1.]),
+                      objlib.add_sphere([-.75, .1, 2.25], .6, [.5, .223, .5]),
+                      objlib.add_sphere([-2.75, .1, 3.5], .6, [1., .572, .184]),
+                      objlib.add_plane([0., -.5, 0.], [0., 1., 0.],self.color_plane0, self.color_plane1),
                     ]
 
         # Light info
